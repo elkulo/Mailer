f37e5406f06c05c6d01da3d97944cbaa41386323
A.Sudo <walkyxwalky@outlook.com>
2021-01-22 21:47:59 +0900

    update

--- public/webpack/src/js/SmoothScroll.js
+++ public/webpack/src/js/SmoothScroll.js
@@ -1,69 +1,75 @@
-/*
- * SmoothScroll@1.0.0
+/**
+ * SmoothScroll.js
+ *
+ * @version 2.0.0
  */
 const SmoothScroll = ( headHeight = 0 ) => {
 
-	// スクロール処理を繰り返す間隔
-	const interval = 10;
+	const run = () => {
 
-	// 近づく割合（数値が大きいほどゆっくり近く）
-	const divisor = 8;
+		// スクロール処理を繰り返す間隔
+		const interval = 10;
 
-	// どこまで近づけば処理を終了するか(無限ループにならないように divisor から算出)
-	const range = ( divisor / 2 ) + 1;
-	const links = document.querySelectorAll( 'a[href*="#"]' );
+		// 近づく割合（数値が大きいほどゆっくり近く）
+		const divisor = 8;
 
-	for ( let i = 0; i < links.length; i++ ) {
-		links[i].addEventListener( 'click', e => {
-			Scrolling( e );
-		}, false );
-	}
+		// どこまで近づけば処理を終了するか(無限ループにならないように divisor から算出)
+		const range = ( divisor / 2 ) + 1;
+		const links = document.querySelectorAll( 'a[href*="#"]' );
 
-	const Scrolling = e => {
-
-		// パスから#までの文字を切り抜いてリンク先の要素を取得
-		const hrefAttr = e.currentTarget.getAttribute( 'href' );
-		const hrefID = hrefAttr.slice( hrefAttr.lastIndexOf( '#' ) );
-		const target = document.querySelector( hrefID );
-
-		// ページ内にアンカー要素がなければ通常の遷移
-		// または、URLが'#'の後に'/'がある場合は別ページとして判定
-		if ( target && hrefAttr.lastIndexOf( '/' ) < hrefAttr.lastIndexOf( '#' ) ) {
-			e.preventDefault();
-		} else {
-			return;
+		for ( let i = 0; i < links.length; i++ ) {
+			links[i].addEventListener( 'click', e => {
+				Scrolling( e );
+			}, false );
 		}
 
-		// 現在のスクロール値
-		let nowY = window.pageYOffset;
-		let toY;
+		const Scrolling = e => {
 
-		// ターゲットの座標取得
-		// 現在のスクロール値 & ヘッダーの高さを踏まえた座標
-		const targetRect = target.getBoundingClientRect();
-		const targetY = targetRect.top + nowY - headHeight;
+			// パスから#までの文字を切り抜いてリンク先の要素を取得
+			const hrefAttr = e.currentTarget.getAttribute( 'href' );
+			const hrefID = hrefAttr.slice( hrefAttr.lastIndexOf( '#' ) );
+			const target = document.querySelector( hrefID );
 
-		// スクロール終了まで繰り返す処理
-		( function Smooth() {
-			toY = nowY + Math.round( ( targetY - nowY ) / divisor );
-			window.scrollTo( 0, toY );
-			nowY = toY;
-
-			// 最下部にスクロールしても対象まで届かない場合は下限までスクロールして強制終了
-			if ( document.body.clientHeight - window.innerHeight < toY ) {
-				window.scrollTo( 0, document.body.clientHeight );
+			// ページ内にアンカー要素がなければ通常の遷移
+			// または、URLが'#'の後に'/'がある場合は別ページとして判定
+			if ( target && hrefAttr.lastIndexOf( '/' ) < hrefAttr.lastIndexOf( '#' ) ) {
+				e.preventDefault();
+			} else {
 				return;
 			}
-			if ( toY >= targetY + range || toY <= targetY - range ) {
 
-				//+-rangeの範囲内へ近くまで繰り返す
-				window.setTimeout( Smooth, interval );
-			} else {
+			// 現在のスクロール値
+			let nowY = window.pageYOffset;
+			let toY;
 
-				//+-range の範囲内にくれば正確な値へ移動して終了。
-				window.scrollTo( 0, targetY );
-			}
-		}() );
+			// ターゲットの座標取得
+			// 現在のスクロール値 & ヘッダーの高さを踏まえた座標
+			const targetRect = target.getBoundingClientRect();
+			const targetY = targetRect.top + nowY - headHeight;
+
+			// スクロール終了まで繰り返す処理
+			( function Smooth() {
+				toY = nowY + Math.round( ( targetY - nowY ) / divisor );
+				window.scrollTo( 0, toY );
+				nowY = toY;
+
+				// 最下部にスクロールしても対象まで届かない場合は下限までスクロールして強制終了
+				if ( document.body.clientHeight - window.innerHeight < toY ) {
+					window.scrollTo( 0, document.body.clientHeight );
+					return;
+				}
+				if ( toY >= targetY + range || toY <= targetY - range ) {
+
+					//+-rangeの範囲内へ近くまで繰り返す
+					window.setTimeout( Smooth, interval );
+				} else {
+
+					//+-range の範囲内にくれば正確な値へ移動して終了。
+					window.scrollTo( 0, targetY );
+				}
+			}() );
+		};
 	};
+	window.addEventListener( 'load', run, false );
 };
 export default SmoothScroll;
