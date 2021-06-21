a771f352f492e89bce1d2288476c5c642c26a591
A.Sudo <50855593+elkulo@users.noreply.github.com>
2021-06-21 21:31:22 +0900

    update

--- public/webpack/src/app.js
+++ public/webpack/src/app.js
@@ -1,3 +1,8 @@
+/*!
+* Mailer | el.kulo v1.0.0 (https://github.com/elkulo/Mailer/)
+* Copyright 2020-2021 A.Sudo
+* Licensed under MIT (https://github.com/elkulo/Mailer/blob/main/LICENSE)
+*/
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import DrawerNavi from './js/DrawerNavi';
