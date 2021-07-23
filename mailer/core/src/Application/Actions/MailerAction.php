cafe37a9749f50121ed2fab8f25ad5c6a63ee687
A.Sudo <walkyxwalky@outlook.com>
2021-07-23 18:24:10 +0900

    update

--- mailer/core/src/Application/Actions/MailerAction.php
+++ mailer/core/src/Application/Actions/MailerAction.php
@@ -10,58 +10,12 @@ namespace App\Application\Actions;
 
 use App\Application\Actions\Action;
 use Psr\Container\ContainerInterface;
-use App\Domain\Mailer;
-use App\Application\Handlers\Validate\ValidateHandler;
-use App\Application\Handlers\View\ViewHandler;
-use App\Application\Handlers\Mail\MailHandlerInterface as MailHandler;
-use App\Application\Handlers\DB\DBHandlerInterface as DBHandler;
 
 /**
  * MailerAction
  */
 class MailerAction extends Action
 {
-
-    /**
-     * @var object
-     */
-    protected $logger;
-
-    /**
-     * ロジック
-     *
-     * @var object
-     */
-    protected $repository;
-
-    /**
-     * バリデート
-     *
-     * @var object
-     */
-    protected $validate;
-
-    /**
-     * Twig ハンドラー
-     *
-     * @var object
-     */
-    protected $view;
-
-    /**
-     * メールハンドラー
-     *
-     * @var object
-     */
-    protected $mail;
-
-    /**
-     * DBハンドラー
-     *
-     * @var object|null
-     */
-    protected $db;
-
     /**
      * コンストラクタ
      *
@@ -71,23 +25,7 @@ class MailerAction extends Action
     public function __construct(ContainerInterface $container)
     {
         try {
-            // ロガーをセット
-            $this->logger = $container->get('logger');
-
-            // ロジックをセット
-            $this->repository = $container->get(Mailer::class);
-
-            // バリデーションアクションをセット
-            $this->validate = $container->get(ValidateHandler::class);
-
-            // ビューアクションをセット
-            $this->view = $container->get(ViewHandler::class);
-
-            // メールハンドラーをセット
-            $this->mail = $container->get(MailHandler::class);
-
-            // データベースハンドラーをセット
-            $this->db = $container->get(DBHandler::class);
+            parent::__construct($container);
 
             // 連続投稿防止
             $this->repository->checkinSession();
