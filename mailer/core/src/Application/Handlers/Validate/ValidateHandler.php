a2a45d018f4f29f4d86bfb5c5d65bf840aceb718
A.Sudo <50855593+elkulo@users.noreply.github.com>
2021-10-01 15:16:02 +0900

    update

--- mailer/core/src/Application/Handlers/Validate/ValidateHandler.php
+++ mailer/core/src/Application/Handlers/Validate/ValidateHandler.php
@@ -1,11 +1,9 @@
 <?php
-
 /**
  * Mailer | el.kulo v3.0.0 (https://github.com/elkulo/Mailer/)
  * Copyright 2020-2021 A.Sudo
  * Licensed under MIT (https://github.com/elkulo/Mailer/blob/main/LICENSE)
  */
-
 declare(strict_types=1);
 
 namespace App\Application\Handlers\Validate;
