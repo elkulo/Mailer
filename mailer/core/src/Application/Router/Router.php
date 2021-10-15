71b480f9236f95beb4da0b3b0d9120df70420afe
A.Sudo <walkyxwalky@outlook.com>
2021-10-16 01:00:01 +0900

    update

--- mailer/core/src/Application/Router/Router.php
+++ mailer/core/src/Application/Router/Router.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Mailer | el.kulo v3.0.0 (https://github.com/elkulo/Mailer/)
+ * Copyright 2020-2021 A.Sudo
+ * Licensed under LGPL-2.1-only (https://github.com/elkulo/Mailer/blob/main/LICENSE)
+ */
 declare(strict_types=1);
 
 namespace App\Application\Router;
--- mailer/core/src/Domain/Mailer/MailerPostData.php
+++ mailer/core/src/Domain/Mailer/MailerPostData.php
@@ -2,7 +2,7 @@
 /**
  * Mailer | el.kulo v3.0.0 (https://github.com/elkulo/Mailer/)
  * Copyright 2020-2021 A.Sudo
- * Licensed under MIT (https://github.com/elkulo/Mailer/blob/main/LICENSE)
+ * Licensed under LGPL-2.1-only (https://github.com/elkulo/Mailer/blob/main/LICENSE)
  */
 declare(strict_types=1);
 
