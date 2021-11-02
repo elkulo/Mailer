7bcc4251ea9584b342c183b6b36cfd5d71b85fe2
A.Sudo <walkyxwalky@outlook.com>
2021-11-02 21:56:17 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,16 @@
 # mailerディレクトリのZipを作成
 zip -r dist/Mailer-latest-ja.zip mailer/ -x '*.git*' '*.env*' '*.DS_Store' '*__MACOSX*' '*.log*' '*/logs/' '*.sql*' '*/database/'
 
-# publicディレクトリをexample名でZipに追加
+# publicディレクトリをexampleに変更
 cp -R public/ example/
+
+# htaccessをhtaccess.txtに変更
+mv -n example/mailer-alias/.htaccess example/mailer-alias/htaccess.txt
+
+# exampleをZipに追加
 zip -r dist/Mailer-latest-ja.zip example/ -x '*.git*' '*.env*' '*.DS_Store' '*__MACOSX*'
+
+# exampleを削除
 rm -rf example
 
 # LICENSEをLICENSE.txtにしてZipに追加
